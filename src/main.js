c15ead63bb14d8518ef4669479bcbf23afb78251
alex <batmandocode@gmail.com>
2021-07-17 12:14:30 +1000

    add element ui

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import helper from '@/plugins/helper'
+import ElementUI from 'element-ui'
 import App from '@/App.vue'
 import '@/plugins/vuetify'
 import '@/components'
 import './plugins/Filters'
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
 Vue.use(helper)
+Vue.use(ElementUI);
 
 new Vue({ render: (h) => h(App) }).$mount('#app')
