94bf5954aed983118bd62952afdb533239051030
alex <batmandocode@gmail.com>
2021-07-27 16:45:43 +1000

    finish the tech stack part

--- src/views/dark-template/sidebar/Container.vue
+++ src/views/dark-template/sidebar/Container.vue
@@ -15,20 +15,45 @@
       <div>
         <info-card :title="`tech stack`">
           <div class="tech-stack-title">
-            <el-button class="tech-stack-btn" type="primary" plain 
-              >Backend</el-button
-            >
+            <span>Backend </span>
           </div>
           <div class="tech-stack-body">
-            <el-button class="tech-stack-btn" type="primary" round
-              >Nodejs</el-button
+            <el-button
+              class="tech-stack-btn"
+              type="info"
+              round
+              v-for="(tech, index) in sections.info.backend"
+              :key="`${tech}-${index}`"
             >
-
-            <el-button class="tech-stack-btn" type="primary" round
-              >GraphQL Server
+              {{ tech }}
             </el-button>
-            <el-button class="tech-stack-btn" type="primary" round
-              >Express Server
+          </div>
+          <div class="tech-stack-title">
+            <span>Frontend </span>
+          </div>
+          <div class="tech-stack-body">
+            <el-button
+              class="tech-stack-btn"
+              type="info"
+              round
+              v-for="(tech, index) in sections.info.frontend"
+              :key="`${tech}-${index}`"
+            >
+              {{ tech }}
+            </el-button>
+          </div>
+          <div class="tech-stack-title">
+            <span>DevOps </span>
+          </div>
+          <div class="tech-stack-body">
+            <el-button
+              class="tech-stack-btn"
+              type="info"
+              round
+              v-for="(tech, index) in sections.info.devops"
+              :key="`${tech}-${index}`"
+            >
+              {{ tech }}
             </el-button>
           </div>
         </info-card>
@@ -38,8 +63,6 @@
 </template>
 
 <script>
-import InfoCard from "../../../components/InfoCard.vue";
-import InfoCardVue from "../../../components/InfoCard.vue";
 import Avatar from "../../../views/dark-template/sidebar/Avatar";
 import SidebarSection from "../../../views/dark-template/sidebar/Section";
 
@@ -54,123 +77,36 @@ export default {
             firstName: "Alex",
             lastName: "Xiang",
           },
-          title: "INFO",
-          items: [
-            {
-              name: "Email",
-              icon: "mdi-email",
-              text: "alex.developer1004@gmail.com",
-            },
-            // {
-            //   name: "Website",
-            //   icon: "mdi-web",
-            //   text: "amirreza.in",
-            // },
-            // {
-            //   name: "Birth Date",
-            //   icon: "mdi-cake-variant",
-            //   text: "Dec 7, 1996",
-            // },
-            {
-              name: "Phone",
-              icon: "mdi-map-marker",
-              text: "Tabriz, Iran",
-            },
+          backend: [
+            "Nodejs",
+            "GraphQL Server",
+            "Express",
+            "Jest",
+            "TDD",
+            "BDD",
+            "Javascript",
+            "Typescript",
           ],
-        },
-        socials: {
-          title: "SOCIALS",
-          items: [
-            {
-              icon: "mdi-github-circle",
-              text: "github.com/AmirrezaNasiri",
-              link: "https://github.com/AmirrezaNasiri",
-            },
-            {
-              icon: "mdi-linkedin-box",
-              text: "linkedin.com/in/amirreza-nasiri",
-              link: "https://linkedin.com/in/amirreza-nasiri",
-            },
-            {
-              icon: "mdi-twitter",
-              text: "twitter.com/Amirreza_Nasiri",
-              link: "https://twitter.com/Amirreza_Nasiri",
-            },
-            {
-              icon: "mdi-instagram",
-              text: "instagram.com/amirreza.n96",
-              link: "https://instagram.com/amirreza.n96",
-            },
+          frontend: [
+            "React",
+            "Redux",
+            "React Router",
+            "Webpack",
+            "CSS Modules",
+            "SASS",
+            "ESLint",
+            "Vue",
+            "Vuex",
+            "Vue-Router",
+            "Vuex-Router-sync",
           ],
-        },
-        hobbies: {
-          title: "HOBBIES",
-          items: [
-            {
-              icon: "mdi-bullseye",
-              text: "Getting out of Safe Zone",
-            },
-            {
-              icon: "mdi-biohazard",
-              text: "Challenges",
-            },
-            {
-              icon: "mdi-bike",
-              text: "Cycling",
-            },
-            {
-              icon: "mdi-image-filter-hdr",
-              text: "Nature",
-            },
-            {
-              icon: "mdi-auto-fix",
-              text: "Hacking Stuffs",
-            },
-            {
-              icon: "mdi-teach",
-              text: "Teaching",
-            },
-            {
-              icon: "mdi-karate",
-              text: "Sports",
-            },
-            {
-              icon: "mdi-music",
-              text: "Music",
-            },
-            {
-              icon: "mdi-account-group",
-              text: "Leadership",
-            },
-            {
-              icon: "mdi-book-open-page-variant",
-              text: "Books",
-            },
-            {
-              icon: "mdi-android-debug-bridge",
-              text: "Tickling Bugs!",
-            },
-          ],
-        },
-        languages: {
-          title: "LANGUAGES",
-          items: [
-            {
-              text: "English",
-              value: 70,
-            },
-            {
-              text: "Turkish",
-              value: 45,
-            },
-            {
-              text: "Azeri",
-              value: 100,
-            },
-            {
-              text: "Persian",
-              value: 100,
-            },
+          devops: [
+            "Google Cloud Platform (GCP)",
+            "Amazon Web Service (AWS)",
+            "Docker",
+            "Kubernetes",
+            "Jenkins",
+            "TreavisCI",
           ],
         },
       },
@@ -188,10 +124,13 @@ export default {
     font-size: 1rem;
   }
   .tech-stack-btn {
-    padding: 0.5rem;
-    margin: 0.2rem;
+    padding: 0.2rem 0.6rem;
+    margin: 0.6rem;
+    min-width: 5rem;
   }
   .tech-stack-title {
+    margin: 0.5rem;
+    font-size: 1.2rem;
   }
   .tech-stack-body {
     display: flex;
